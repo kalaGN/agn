e83d32adca74c71cdc48e0ccbbc1497c4c106a3c
kalaGN <491010750@qq.com>
2022-05-16 15:55:44 +0800

    database logger chang

--- bootstrap/database.go
+++ bootstrap/database.go
@@ -5,6 +5,7 @@ import (
 	"agn/app/models/user"
 	"agn/pkg/config"
 	"agn/pkg/database"
+	"agn/pkg/logger"
 	"errors"
 	"fmt"
 	"time"
@@ -12,7 +13,6 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
 
 // SetupDB 初始化数据库和 ORM
@@ -42,7 +42,7 @@ func SetupDB() {
 	}
 
 	// 连接数据库，并设置 GORM 的日志模式
-	database.Connect(dbConfig, logger.Default.LogMode(logger.Info))
+	database.Connect(dbConfig, logger.NewGormLogger())
 
 	// 设置最大连接数
 	database.SQLDB.SetMaxOpenConns(config.GetInt("database.mysql.max_open_connections"))
