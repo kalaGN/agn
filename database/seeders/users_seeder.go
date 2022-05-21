3f5137e6d9c653b97ac873004f5f3db03e915d92
kalaGN <491010750@qq.com>
2022-05-21 16:00:10 +0800

    add seed  userseed

---
+++ database/seeders/users_seeder.go
@@ -0,0 +1,33 @@
+package seeders
+
+import (
+	"agn/database/factories"
+	"agn/pkg/console"
+	"agn/pkg/logger"
+	"agn/pkg/seed"
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+func init() {
+
+	// 添加 Seeder
+	seed.Add("SeedUsersTable", func(db *gorm.DB) {
+
+		// 创建 10 个用户对象
+		users := factories.MakeUsers(10)
+
+		// 批量创建用户（注意批量创建不会调用模型钩子）
+		result := db.Table("user").Create(&users)
+
+		// 记录错误
+		if err := result.Error; err != nil {
+			logger.LogIf(err)
+			return
+		}
+
+		// 打印运行情况
+		console.Success(fmt.Sprintf("Table [%v] %v rows seeded", result.Statement.Table, result.RowsAffected))
+	})
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module agn
 go 1.18
 
 require (
+	github.com/bxcodec/faker/v3 v3.8.0
 	github.com/gertd/go-pluralize v0.2.1
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-redis/redis/v8 v8.11.5
@@ -22,7 +23,6 @@ require (
 )
 
 require (
-	github.com/bxcodec/faker/v3 v3.8.0 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
